552724e25c4d417e41710a53acbd4d42393a6b22
manoj149 <reachmanoj236@gmail.com>
2018-09-08 18:57:28 +0530

    first commit

---
+++ src/main/java/com/java/spring/User.java
@@ -0,0 +1,24 @@
+package com.java.spring;
+
+public class User {
+
+	private String userName;
+	private String emailID;
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	
+}
